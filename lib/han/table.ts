1768813148380a57830b3defb6025569bf378fe6
bluelovers <codelovers@users.sourceforge.net>
2017-12-24 17:59:02 +0800

    增加 中日漢字轉換 (Step: 2)
    Step: 2
    
    [M ] modified:
    
    	lib/han/zh2num.ts
    
    [A ] new file:
    
    	lib/han/table.ts
    	lib/han/teachKanjiComparison.cache.json
    	lib/han/teachKanjiComparison.json
    	lib/han/zh2jp.ts
    
    [ M] modified - Changes not staged for commit:
    
    	lib/util.ts

---
+++ lib/han/table.ts
@@ -0,0 +1,38 @@
+/**
+ * Created by user on 2017/12/24/024.
+ */
+
+/**
+ * 資料來源 https://www.jpmarumaru.com/tw/teachKanjiComparison.asp
+ * @see https://www.jpmarumaru.com/tw/teachKanjiComparison.asp
+ */
+const teachKanjiComparison = require('./teachKanjiComparison.json') as string[][];
+
+export let TABLE = teachKanjiComparison;
+
+export function addNew(table: string[][], jp, zht, zhs): string[][]
+{
+	jp = Array.isArray(jp) ? jp : [jp];
+	zht = Array.isArray(zht) ? zht : [zht];
+	zhs = Array.isArray(zhs) ? zhs : [zhs];
+
+	table.push([
+		jp,
+		zht,
+		zhs,
+	]);
+
+	return table;
+}
+
+[
+	[
+		'蝕', '蝕', '蚀'
+	],
+].forEach(function ([jp, zht, zhs])
+{
+	addNew(TABLE, jp, zht, zhs);
+});
+
+export default TABLE;
+//export default exports;
