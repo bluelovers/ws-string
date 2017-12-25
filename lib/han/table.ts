1fae6ddb90a5c8a2f9a6417d5406fe98f6df54a9
bluelovers <codelovers@users.sourceforge.net>
2017-12-25 13:22:58 +0800

    . (Step: 26)
    Step: 26
    
    [M ] modified:
    
    	docs/globals.html
    	docs/index.html
    	docs/interfaces/_lib_fullhalf_.fullhalfcore.ioptions.html
    	docs/interfaces/_lib_fullhalf_.fullhalfcore.ioptionstype.html
    	docs/interfaces/_lib_fullhalf_.fullhalfcore.itable.html
    	docs/interfaces/_lib_fullhalf_.fullhalfcore.itableobject.html
    	docs/interfaces/_lib_han_zh2num_.ioptions.html
    	docs/interfaces/_lib_util_.icharcodeatfn.html
    	docs/modules/_index_.html
    	docs/modules/_lib_fullhalf_.fullhalfcore.html
    	docs/modules/_lib_fullhalf_.html
    	docs/modules/_lib_han_zh2num_.html
    	docs/modules/_lib_is_fullwidth_.html
    	docs/modules/_lib_util_.html
    	docs/modules/_lib_var_.html
    	docs/modules/_lib_width_.html
    
    [A ] new file:
    
    	docs/interfaces/_lib_han_zh2jp_.ifrom2to.html
    	docs/interfaces/_lib_han_zh2jp_.ioptions.html
    	docs/interfaces/_lib_han_zh2jp_.itable.html
    	docs/interfaces/_lib_han_zh2jp_.itablesub.html
    	docs/modules/_lib_han_table_.html
    	docs/modules/_lib_han_zh2jp_._.html
    	docs/modules/_lib_han_zh2jp_.html
    
    [ M] modified - Changes not staged for commit:
    
    	README.md
    	index.ts
    	lib/han/table.ts

--- lib/han/table.ts
+++ lib/han/table.ts
@@ -1,5 +1,11 @@
 /**
  * Created by user on 2017/12/24/024.
+ *
+ * @see https://www.jcinfo.net/tw/tools/kanji
+ * @see http://dict.variants.moe.edu.tw/variants/rbt/japan_chinese_character_tiles.rbt?pageId=2981908
+ * @see https://en.wikipedia.org/wiki/List_of_j%C5%8Dy%C5%8D_kanji
+ * @see https://hanzi.unihan.com.cn/CJKCompare
+ * @see http://www5b.biglobe.ne.jp/%7Eharigaya/variants.html
  */
 
 /**
@@ -26,9 +32,8 @@ export function addNew(table: string[][], jp, zht, zhs): string[][]
 }
 
 [
-	[
-		'蝕', '蝕', '蚀'
-	],
+	['蝕', '蝕', '蚀'],
+	//['戻', '戾', null],
 ].forEach(function ([jp, zht, zhs])
 {
 	addNew(TABLE, jp, zht, zhs);
