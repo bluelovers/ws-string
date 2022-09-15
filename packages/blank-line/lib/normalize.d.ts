405045666f25e3433f213e5ae0beca4f3460b578
bluelovers <codelovers@users.sourceforge.net>
2022-09-16 06:59:13 +0800

    .

--- packages/blank-line/lib/normalize.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by user on 2018/1/19/019.
- */
-/// <reference types="node" />
-export declare const LF = "\n";
-export declare function normalize(txt: string | Buffer): string;
-export default normalize;
